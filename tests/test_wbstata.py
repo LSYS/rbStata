4eff0959b7b2056313b87a90705b1ae4bd10f255
Lucas Shen Y. S <lucas@lucasshen.com>
2022-09-11 22:05:47 +0800

    debugging CI error (#5 #18)

--- tests/test_wbstata.py
+++ tests/test_wbstata.py
@@ -119,12 +119,13 @@ def test_wbstata():
     result = runner.invoke(wbstata)
     assert result.exit_code == 0
     assert "> .dta file(s)" in result.output
-    # assert "You will be prompted for relevant settings." in result.output
-    # assert "Please enter values under the following settings." in result.output
-    # assert (
-    #     "(just press Enter to accept the default value in brackets)"
-    #     in result.output
-    # )
+    assert "Welcome to the wbStata quickstart command-line utility." in result.output
+    assert "You will be prompted for relevant settings." in result.output
+    assert "Please enter values under the following settings." in result.output
+    assert (
+        "(just press Enter to accept the default value in brackets)"
+        in result.output
+    )
 
     # Check that prompt works with just file (prompt for version)
     result = runner.invoke(wbstata, ["datasets/census.dta"])
