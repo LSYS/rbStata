fb0e7029975a44096bcf4893a8fbb2b50873cf1b
LSYS <lucas@lucasshen.com>
2024-04-04 14:08:04 +0800

    #57

--- setup.py
+++ setup.py
@@ -7,36 +7,34 @@ with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 install_requires = [
-    # 'pandas',
+    "pandas",
     "anyascii",
     "click>=8.*",
 ]
 
 setup(
-    name="wbstata",
+    name="rbstata",
     version=version,
     description=(
-        "A command-line utility that creates projects from project "
-        "templates, e.g. creating a Python package project from a "
-        "Python package project template."
+        "A command-line utility that converts (or roll back) versions of Stata dta data files."
     ),
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Lucas Shen",
     author_email="lucas@lucasshen.com",
-    url="https://github.com/lsys/wbStata",
+    url="https://github.com/lsys/rbStata",
     packages=find_packages(),
-    entry_points={"console_scripts": ["wbstata = wbStata.cli:wbstata"]},
+    entry_points={"console_scripts": ["rbstata = rbStata.cli:rbstata"]},
     include_package_data=True,
     python_requires=">=3.7",
     install_requires=install_requires,
-    license="BSD",
+    license="MIT",
     zip_safe=False,
     classifiers=[
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Natural Language :: English",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
@@ -47,15 +45,10 @@ setup(
         "Topic :: Software Development",
     ],
     keywords=[
-        "cookiecutter",
+        "Stata",
         "Python",
-        "projects",
-        "project templates",
-        "Jinja2",
-        "skeleton",
-        "scaffolding",
-        "project directory",
-        "package",
-        "packaging",
+        "cli",
+        "compatibility",
+        "version",
     ],
 )
